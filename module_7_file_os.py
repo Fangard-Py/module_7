d5000d2eb70de39bc3bee3833c8aeb26b22031b3
Fangard-Py <pahan89reg@gmail.com>
2024-09-29 16:14:09 +0500

    Update module_7_file_os.py

--- module_7_file_os.py
+++ module_7_file_os.py
@@ -1,14 +1,20 @@
 import os
 import time
 
-directory = '.'
+
+def print_file_info(filepath):
+    filetime = os.path.getmtime(filepath)
+    formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+    filesize = os.path.getsize(filepath)
+    parent_dir = os.path.dirname(filepath)
+
+    print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт,'
+          f' Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+
+
+directory = os.getcwd()
+
 for root, dirs, files in os.walk(directory):
     for file in files:
         filepath = os.path.join(root, file)
-        filetime = os.path.getmtime(filepath)
-        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        filesize = os.path.getsize(filepath)
-        parent_dir = os.path.dirname(filepath)
-
-print(
-    f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+        print_file_info(filepath)
